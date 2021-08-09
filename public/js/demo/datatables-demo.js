1fc7d3e431b32edb2af0c57c578250c6c90b03f1
J3C118045 <72448563+J3C118045@users.noreply.github.com>
2021-08-09 12:10:43 +0700

    Add files via upload

---
+++ public/js/demo/datatables-demo.js
@@ -0,0 +1,12 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+  $('#dataTable').DataTable({
+    responsive: true,
+  });
+//   $('#reportTable').DataTable( {
+//     dom: 'Bfrtip',
+//     buttons: [
+//         'copy', 'csv', 'excel', 'pdf', 'print'
+//     ]
+// } );
+});
